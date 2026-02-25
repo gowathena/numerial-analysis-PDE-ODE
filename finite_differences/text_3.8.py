9480401ca5eb69ff862f6a065be44f4548aa4a33
Athena Zhong <athenazh@sas.upenn.edu>
2026-02-24 22:21:11 -0500

    adjuested question 8

--- finite_differences/text_3.8.py
+++ finite_differences/text_3.8.py
@@ -1,47 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = lambda x: 100/(np.sqrt(x)+0.01) + 1/((x-0.3)**2+0.001) - np.pi
+def f(x):
+    return 100/np.sqrt(x + 0.01) + 1/((x - 0.3)**2 + 0.001) - np.pi
 
-def trap(x, fx):
-    h = x[1]-x[0]
-    return h*(0.5*fx[0] + np.sum(fx[1:-1]) + 0.5*fx[-1])
+def trap(fx, h):
+    return h * (0.5*fx[0] + fx[1:-1].sum() + 0.5*fx[-1])
 
-def simpson(x, fx):
-    n = len(x)-1
-    h = x[1]-x[0]
+def simpson(fx, h):
+    n = len(fx) - 1
+    if n % 2:
+        raise ValueError("Simpson needs even n.")
     w = np.ones(n+1)
     w[1:-1:2] = 4
     w[2:-1:2] = 2
-    return (h/3)*np.dot(w, fx)
+    return (h/3) * (w @ fx)
 
-def end_corrected(x, fx):
-    h = x[1]-x[0]
-    IT = trap(x, fx)
-    f0,f1,f2,f3,f4 = fx[:5]
-    fn,fn1,fn2,fn3,fn4 = fx[-1],fx[-2],fx[-3],fx[-4],fx[-5]
-    fp0 = (-25*f0+48*f1-36*f2+16*f3-3*f4)/(12*h)
-    fp1 = (25*fn-48*fn1+36*fn2-16*fn3+3*fn4)/(12*h)
-    return IT - (h**2/12)*(fp1-fp0)
+def end_corr_trap(fx, h):
+    IT = trap(fx, h)
 
-# reference (fine Simpson)
+    f0, f1, f2, f3, f4 = fx[:5]
+    fn, fn1, fn2, fn3, fn4 = fx[-1], fx[-2], fx[-3], fx[-4], fx[-5]
+
+    fp0 = (-25*f0 + 48*f1 - 36*f2 + 16*f3 - 3*f4) / (12*h)
+    fp1 = ( 25*fn - 48*fn1 + 36*fn2 - 16*fn3 + 3*fn4) / (12*h)
+
+    return IT - (h**2/12) * (fp1 - fp0)
+
+def compute_rules(n):
+    x = np.linspace(0.0, 1.0, n+1)
+    fx = f(x)
+    h = 1.0/n
+    return (
+        trap(fx, h),
+        simpson(fx, h),
+        end_corr_trap(fx, h),
+    )
+
+# reference integral (fine Simpson)
 n_ref = 2**16
-x_ref = np.linspace(0,1,n_ref+1)
-I_ref = simpson(x_ref, f(x_ref))
+I_ref = compute_rules(n_ref)[1]
 
-ns = [2**k for k in range(3,11)]
+ns = 2**np.arange(3, 11)  # 8..1024
 errT, errS, errTc = [], [], []
 
 for n in ns:
-    x = np.linspace(0,1,n+1)
-    fx = f(x)
-    errT.append(abs(trap(x,fx)-I_ref)/abs(I_ref))
-    errS.append(abs(simpson(x,fx)-I_ref)/abs(I_ref))
-    errTc.append(abs(end_corrected(x,fx)-I_ref)/abs(I_ref))
+    IT, IS, ITc = compute_rules(int(n))
+    errT.append(abs(IT  - I_ref)/abs(I_ref))
+    errS.append(abs(IS  - I_ref)/abs(I_ref))
+    errTc.append(abs(ITc - I_ref)/abs(I_ref))
 
-plt.loglog(ns, errT, 'o-', label="Trap")
-plt.loglog(ns, errS, 's-', label="Simpson")
-plt.loglog(ns, errTc, 'd-', label="Trap+end")
+plt.loglog(ns, errT,  "o-", label="Trap")
+plt.loglog(ns, errS,  "s-", label="Simpson")
+plt.loglog(ns, errTc, "d-", label="Trap + end")
+plt.xlabel("n (panels)")
+plt.ylabel("relative error")
+plt.grid(True, which="both")
 plt.legend()
-plt.grid(True)
 plt.show()
--- finite_differences/text_3.8c.py
+++ finite_differences/text_3.8c.py
@@ -1,78 +1,54 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def f(x):
-    return 100/(np.sqrt(x)+0.01) + 1/((x-0.3)**2+0.001) - np.pi
+    return 100/np.sqrt(x + 0.01) + 1/((x - 0.3)**2 + 0.001) - np.pi
 
-def S(a,b,fa,fm,fb):
-    return (b-a)*(fa + 4*fm + fb)/6
+def S(a, b, fa, fm, fb):
+    return (b - a) * (fa + 4*fm + fb) / 6.0
 
 def adaptive_simpson(a=0.0, b=1.0, tol=1e-8, max_depth=30):
-    xs = []
+    xs = [a, 0.5*(a+b), b]  
 
-    def rec(a,b,fa,fm,fb,Sab,tol,depth):
-        m = 0.5*(a+b)
-        l = 0.5*(a+m)
-        r = 0.5*(m+b)
+    def rec(a, b, fa, fm, fb, Sab, tol, depth):
+        m = 0.5*(a + b)
+        l = 0.5*(a + m)
+        r = 0.5*(m + b)
 
         fl, fr = f(l), f(r)
         xs.extend([l, r])
 
-        Sleft  = S(a,m,fa,fl,fm)
-        Sright = S(m,b,fm,fr,fb)
+        Sleft  = S(a, m, fa, fl, fm)
+        Sright = S(m, b, fm, fr, fb)
 
-        if depth >= max_depth or abs(Sleft + Sright - Sab) < 15*tol:
-            return Sleft + Sright + (Sleft + Sright - Sab)/15.0
+        err = Sleft + Sright - Sab
 
-        return rec(a,m,fa,fl,fm,Sleft,tol/2,depth+1) + rec(m,b,fm,fr,fb,Sright,tol/2,depth+1)
+        if depth >= max_depth or abs(err) < 15*tol:
+            return Sleft + Sright + err/15.0
+
+        return (rec(a, m, fa, fl, fm, Sleft,  tol/2, depth+1) +
+                rec(m, b, fm, fr, fb, Sright, tol/2, depth+1))
 
     fa, fb = f(a), f(b)
-    m = 0.5*(a+b)
+    m = 0.5*(a + b)
     fm = f(m)
-    xs.extend([a, m, b])
 
-    I0 = S(a,b,fa,fm,fb)
-    I = rec(a,b,fa,fm,fb,I0,tol,0)
+    I0 = S(a, b, fa, fm, fb)
+    I = rec(a, b, fa, fm, fb, I0, tol, 0)
 
     return I, np.unique(xs)
 
-'''
-import numpy as np
-import matplotlib.pyplot as plt
 
-def f(x):
-    return 100/(np.sqrt(x)+0.01) + 1/((x-0.3)**2+0.001) - np.pi
+I, xs = adaptive_simpson(a=0.0, b=1.0, tol=1e-8)
 
-def S(a,b,fa,fm,fb):
-    return (b-a)*(fa+4*fm+fb)/6
-
-def adaptive(a,b,tol):
-    xs = []
-    def rec(a,b,fa,fm,fb,Sab,tol):
-        c = (a+b)/2
-        d = (a+c)/2
-        e = (c+b)/2
-        fd,fe = f(d),f(e)
-        xs.extend([d,e])
-        Sleft  = S(a,c,fa,fd,fm)
-        Sright = S(c,b,fm,fe,fb)
-        if abs(Sleft+Sright-Sab) < 15*tol:
-            return Sleft+Sright
-        return rec(a,c,fa,fd,fm,Sleft,tol/2) + rec(c,b,fm,fe,fb,Sright,tol/2)
-
-    fa, fb = f(a), f(b)
-    m = (a+b)/2
-    fm = f(m)
-    xs.extend([a,m,b])
-    I = rec(a,b,fa,fm,fb,S(a,b,fa,fm,fb),tol)
-    return I, np.unique(xs)
-
-I, xs = adaptive(0,1,1e-8)
 print("Adaptive I =", I)
 
-x_plot = np.linspace(0,1,2000)
-plt.plot(x_plot, f(x_plot))
-plt.plot(xs, f(xs), 'o', markersize=3)
-plt.grid()
-plt.show()
+x_plot = np.linspace(0, 1, 2000)
 
-'''
+plt.plot(x_plot, f(x_plot), label="f(x)")
+plt.plot(xs, f(xs), 'o', markersize=3, label="evaluation points")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.legend()
+plt.grid(True)
+plt.show()
